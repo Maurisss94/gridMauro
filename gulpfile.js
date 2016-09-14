3ed678aa9a7f5a4c80dc393b2817ea74c61eeb2b
articstudio <IhpMpOTetokWBzq0oPEr>
2016-09-14 18:29:05 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/**
+ * Created by mauro on 9/09/16.
+ */
+'use strict'
+
+var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    runSequence = require('run-sequence'),
+    config = {
+        sass: {
+            source: 'sass/main.scss',
+            target: 'dist/'
+        },
+        minify: {
+            source: ['dist/*.css', '!dist/*.min.css'],
+            target: 'dist/'
+        }
+    };
+
+gulp.task('sass', function () {
+    return gulp.src(config.sass.source, {read: true})
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(config.sass.target));
+});
+gulp.task('minify', function () {
+    return gulp.src(config.minify.source)
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.minify.target));
+});
+
+gulp.task('make', function (cb) {
+    runSequence(
+        'sass',
+        'minify',
+        cb
+    );
+});
+
